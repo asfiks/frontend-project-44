dbde7302807846ef3c7a869db948dac4e41599e5
asfiks <asfiks88@gmail.com>
2022-12-24 12:43:35 +0300

    fix brain-progression

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -5,14 +5,15 @@ const gameRule = 'What number is missing in the progression?';
 const gameQuestionAndAnswer = () => {
   const pace = getRandomNumber(2, 50);
   let result = getRandomNumber(0, 50);
-  const question = [];
-  for (let i = 0; i < getRandomNumber(5, 9); i += 1) {
-    question.push(result);
+  const questionArr = [];
+  for (let i = 0; i < getRandomNumber(6, 9); i += 1) {
+    questionArr.push(result);
     result += pace;
   }
-  const numberElement = getRandomNumber(0, question.length);
-  const answer = question[numberElement];
-  question[numberElement] = '..';
+  const numberElement = getRandomNumber(0, questionArr.length);
+  const answer = questionArr[numberElement];
+  questionArr[numberElement] = '..';
+  const question = questionArr.join(' ');
   return [question, String(answer)];
 };
 
